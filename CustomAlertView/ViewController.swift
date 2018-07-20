6443ab287705cd2a86ddda48e11956ce4381e649
Jackdes <phuong.binh0310@gmail.com>
2018-07-20 11:11:26 +0700

    Initial Commit

---
+++ CustomAlertView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomAlertView
+//
+//  Created by Đái Phương Bình on 7/20/18.
+//  Copyright © 2018 Đái Phương Bình. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
