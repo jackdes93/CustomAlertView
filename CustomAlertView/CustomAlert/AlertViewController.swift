c14ccc43282af2781f8dc582cef57c4539cba0af
Jackdes <phuong.binh0310@gmail.com>
2018-07-22 17:04:08 +0700

    alert view

---
+++ CustomAlertView/CustomAlert/AlertViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AlertViewController.swift
+//  CustomAlertView
+//
+//  Created by Đái Phương Bình on 7/20/18.
+//  Copyright © 2018 Đái Phương Bình. All rights reserved.
+//
+
+import Foundation
---
+++ CustomAlertView/CustomAlert/alertAction.swift
@@ -0,0 +1,9 @@
+//
+//  alertAction.swift
+//  CustomAlertView
+//
+//  Created by Đái Phương Bình on 7/20/18.
+//  Copyright © 2018 Đái Phương Bình. All rights reserved.
+//
+
+import Foundation
